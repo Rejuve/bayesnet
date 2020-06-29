c790837054f73c240077e3bf214dab30312df23e
Deborah Duong <deborahvakasduong@gmail.com>
2020-06-29 14:28:34 -0700

    Update utils.py

--- bayes/utils.py
+++ bayes/utils.py
@@ -1,57 +1,66 @@
-
+from pomegranate import DiscreteDistribution
 
 def any(bayesianNetwork, cpt, invars, outvars):
-    print("bayesianNetwork")
-    print(bayesianNetwork)
-    print("cpt")
-    print(cpt)
-    print ("invars")
-    print (invars)
-    print ("outvars")
-    print (outvars)
+    #print("bayesianNetwork")
+    #print(bayesianNetwork)
+    #print("cpt")
+    #print(cpt)
+    #print ("invars")
+    #print (invars)
+    #print ("outvars")
+    #print (outvars)
     return None
 
 def all(bayesianNetwork, cpt, invars, outvars):
-    print("bayesianNetwork")
-    print(bayesianNetwork)
-    print("cpt")
-    print(cpt)
-    print("invars")
-    print(invars)
-    print("outvars")
-    print(outvars)
+    #print("bayesianNetwork")
+    #print(bayesianNetwork)
+    #print("cpt")
+    #print(cpt)
+    #print("invars")
+    #print(invars)
+    #print("outvars")
+    #print(outvars)
     return None
 
 def avg(bayesianNetwork, cpt, invars, outvars):
-    print("bayesianNetwork")
-    print(bayesianNetwork)
-    print("cpt")
-    print(cpt)
-    print ("invars")
-    print (invars)
-    print ("outvars")
-    print (outvars)
+    #print("bayesianNetwork")
+    #print(bayesianNetwork)
+    #print("cpt")
+    #print(cpt)
+    #print ("invars")
+    #print (invars)
+    #print ("outvars")
+    #print (outvars)
     return None
 
 def if_then_else(bayesianNetwork, cpt, invars, outvars):
-    print("bayesianNetwork")
-    print(bayesianNetwork)
-    print("cpt")
-    print(cpt)
-    print ("invars")
-    print (invars)
-    print ("outvars")
-    print (outvars)
+    #print("bayesianNetwork")
+    #print(bayesianNetwork)
+    #print("cpt")
+    #print(cpt)
+    #print ("invars")
+    #print (invars)
+    #print ("outvars")
+    #print (outvars)
     return None
 
 def addCpt(bayesianNetwork, cpt):
-    print("bayesianNetwork")
-    print(bayesianNetwork)
-    print("cpt")
-    print(cpt)
+    #print("bayesianNetwork")
+    #print(bayesianNetwork)
+    #print("cpt")
+    #print(cpt)
+    pass
 
 
 def bayesInitialize(bayesianNetwork):
     print("bayesianNetwork")
-    print(bayesianNetwork)
+    for dist in bayesianNetwork:
+        print ("dist.name")
+        print (dist.name)
+        for var in dist:
+            print ("var.name")
+            print (var.name)
+            print ("var.probability")
+            print (var.probability)
+
     return None
