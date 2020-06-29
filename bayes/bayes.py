0d44c6ada9f33e9a6dfeab850080ada031a38934
Deborah Duong <deborahvakasduong@gmail.com>
2020-06-29 01:46:57 -0700

    Add files via upload

---
+++ bayes/bayes.py
@@ -0,0 +1,51 @@
+from pomegranate import *
+
+def any(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print ("invars")
+    print (invars)
+    print ("outvars")
+    print (outvars)
+    return None
+
+def all(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print("invars")
+    print(invars)
+    print("outvars")
+    print(outvars)
+    return None
+
+def avg(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print ("invars")
+    print (invars)
+    print ("outvars")
+    print (outvars)
+    return None
+
+def if_then_else(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print ("invars")
+    print (invars)
+    print ("outvars")
+    print (outvars)
+    return None
+
+def addCpt(bayesianNetwork, cpt):
+    pass
+
+def bayesInitialize(bayesianNetwork):
+    return None
