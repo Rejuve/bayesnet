21555184d690ae57366e109b69c4ff6deddd9eee
Deborah Duong <deborahvakasduong@gmail.com>
2020-06-29 02:19:27 -0700

    Create bayes.py

---
+++ bayes/bayes.py
@@ -0,0 +1,57 @@
+from pomegranate import *
+
+def any(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print ("invars")
+    print (invars)
+    print ("outvars")
+    print (outvars)
+    return None
+
+def all(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print("invars")
+    print(invars)
+    print("outvars")
+    print(outvars)
+    return None
+
+def avg(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print ("invars")
+    print (invars)
+    print ("outvars")
+    print (outvars)
+    return None
+
+def if_then_else(bayesianNetwork, cpt, invars, outvars):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+    print ("invars")
+    print (invars)
+    print ("outvars")
+    print (outvars)
+    return None
+
+def addCpt(bayesianNetwork, cpt):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    print("cpt")
+    print(cpt)
+
+
+def bayesInitialize(bayesianNetwork):
+    print("bayesianNetwork")
+    print(bayesianNetwork)
+    return None
