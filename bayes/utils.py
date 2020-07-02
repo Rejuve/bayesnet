f78557f580a915f834a1f97f4c2b0f596f84dd77
Deborah Duong <deborahvakasduong@gmail.com>
2020-07-01 20:12:50 -0700

    Update utils.py

--- bayes/utils.py
+++ bayes/utils.py
@@ -3,196 +3,202 @@ from pomegranate import BayesianNetwork
 from pomegranate import Node
 from pomegranate import ConditionalProbabilityTable
 
-def dictVarsAndValues(bayesianNetwork,cpt):
-    varsAndValues ={}
-    for dist in bayesianNetwork.discreteDistributions:
-        varsAndValues [dist.name]= []
-        for var in dist.variables:
-            varsAndValues[dist.name].append(var.name)
-    for name,cpt_tuple in cpt.items():
-        print('name')
-        print(name)
-        print('cpt_tuple')
-        print(cpt_tuple)
-        varsAndValues[name]= cpt_tuple[2]
-    return varsAndValues
+class Bayesnet():
+    
+    def __init__():
+        pass()
+    
+    def dictVarsAndValues(bayesianNetwork,cpt):
+        varsAndValues ={}
+        for dist in bayesianNetwork.discreteDistributions:
+            varsAndValues [dist.name]= []
+            for var in dist.variables:
+                varsAndValues[dist.name].append(var.name)
+        for name,cpt_tuple in cpt.items():
+            print('name')
+            print(name)
+            print('cpt_tuple')
+            print(cpt_tuple)
+            varsAndValues[name]= cpt_tuple[2]
+        return varsAndValues
 
-def any(bayesianNetwork, cpt, invars, outvars):
-    import itertools
+    def any(bayesianNetwork, cpt, invars, outvars):
+        import itertools
 
-    vdict = dictVarsAndValues(bayesianNetwork, cpt)
-    vlist = [vdict[v] for v in invars.keys()]
-    cartesian = list(itertools.product(*vlist))
-    klist = [a for a in invars.values()]
-    keylist = invars.keys()
-    cpt_rows = []
-    for c in cartesian:
-        qany=False
-        i=0
-        while (not qany) and i < len(klist):
-            vset = klist[i]
-            i += 1
-            if c[i] in vset:
-                qany = True
-        for i,o in enumerate(outvars):
-            cpt_row = []
-            cpt_row.extend(c)
-            cpt_row.append(o)
-            val = 1.0 if (i == 0 and qany) or (i == 1 and not qany) else 0.0
-            cpt_row.append(val)
-            cpt_rows.append(cpt_row)
-    return (cpt_rows,keylist, outvars)
-
-
-
-def all(bayesianNetwork, cpt, invars, outvars):
-    import itertools
-
-    vdict = dictVarsAndValues(bayesianNetwork, cpt)
-    vlist = [vdict[v] for v in invars.keys()]
-    cartesian = list(itertools.product(*vlist))
-    klist = [a for a in invars.values()]
-    keylist = invars.keys()
-    cpt_rows = []
-    for c in cartesian:
-        qall=True
-        for i,vset in enumerate(klist):
-                if c[i] not in vset:
-                    qall = False
-        for i,o in enumerate(outvars):
-            cpt_row = []
-            cpt_row.extend(c)
-            cpt_row.append(o)
-            val = 1.0 if (i == 0 and qall) or (i == 1 and not qall) else 0.0
-            cpt_row.append(val)
-            cpt_rows.append(cpt_row)
-    return (cpt_rows,keylist,outvars)
-
-
-def avg(bayesianNetwork, cpt, invars, outvars):
-    import itertools
-
-    vdict = dictVarsAndValues(bayesianNetwork, cpt)
-    vlist = [vdict[v] for v in invars.keys()]
-    cartesian = list(itertools.product(*vlist))
-    klist = [a for a in invars.values()]
-    keylist = invars.keys()
-    cpt_rows = []
-    for c in cartesian:
-        qany=False
-        for i,vset in enumerate(klist):
+        vdict = dictVarsAndValues(bayesianNetwork, cpt)
+        vlist = [vdict[v] for v in invars.keys()]
+        cartesian = list(itertools.product(*vlist))
+        klist = [a for a in invars.values()]
+        keylist = invars.keys()
+        cpt_rows = []
+        for c in cartesian:
+            qany=False
+            i=0
+            while (not qany) and i < len(klist):
+                vset = klist[i]
+                i += 1
                 if c[i] in vset:
                     qany = True
-        for i,o in enumerate(outvars):
-            cpt_row = []
-            cpt_row.extend(c)
-            cpt_row.append(o)
-            val = 1.0 if (i == 0 and qany) or (i == 1 and not qany) else 0.0
-            cpt_row.append(val)
-            cpt_rows.append(cpt_row)
-    return (cpt_rows,keylist,outvars)
+            for i,o in enumerate(outvars):
+                cpt_row = []
+                cpt_row.extend(c)
+                cpt_row.append(o)
+                val = 1.0 if (i == 0 and qany) or (i == 1 and not qany) else 0.0
+                cpt_row.append(val)
+                cpt_rows.append(cpt_row)
+        return (cpt_rows,keylist, outvars)
+
+
+
+    def all(bayesianNetwork, cpt, invars, outvars):
+        import itertools
+
+        vdict = dictVarsAndValues(bayesianNetwork, cpt)
+        vlist = [vdict[v] for v in invars.keys()]
+        cartesian = list(itertools.product(*vlist))
+        klist = [a for a in invars.values()]
+        keylist = invars.keys()
+        cpt_rows = []
+        for c in cartesian:
+            qall=True
+            for i,vset in enumerate(klist):
+                    if c[i] not in vset:
+                        qall = False
+            for i,o in enumerate(outvars):
+                cpt_row = []
+                cpt_row.extend(c)
+                cpt_row.append(o)
+                val = 1.0 if (i == 0 and qall) or (i == 1 and not qall) else 0.0
+                cpt_row.append(val)
+                cpt_rows.append(cpt_row)
+        return (cpt_rows,keylist,outvars)
+
+
+    def avg(bayesianNetwork, cpt, invars, outvars):
+        import itertools
+
+        vdict = dictVarsAndValues(bayesianNetwork, cpt)
+        vlist = [vdict[v] for v in invars.keys()]
+        cartesian = list(itertools.product(*vlist))
+        klist = [a for a in invars.values()]
+        keylist = invars.keys()
+        cpt_rows = []
+        bins = {}
+        for c in cartesian:
+            qany=False
+            for i,vset in enumerate(klist):
+                    if c[i] in vset:
+                        qany = True
+            for i,o in enumerate(outvars):
+                cpt_row = []
+                cpt_row.extend(c)
+                cpt_row.append(o)
+                val = 1.0 if (i == 0 and qany) or (i == 1 and not qany) else 0.0
+                cpt_row.append(val)
+                cpt_rows.append(cpt_row)
+        return (cpt_rows,keylist,outvars)
 
 
 
 
-def if_then_else(bayesianNetwork, cpt, invars, outvars):
-    import itertools
+    def if_then_else(bayesianNetwork, cpt, invars, outvars):
+        import itertools
 
-    vdict = dictVarsAndValues(bayesianNetwork, cpt)
-    vlist = [vdict[v] for v in invars.keys()]
-    cartesian = list(itertools.product(*vlist))
-    klist = [a for a in invars.values()]
-    print('cartesian')
-    print(cartesian)
-    print('klist')
-    print(klist)
-    keylist = invars.keys()
-    cpt_rows = [] 
-    for c in cartesian:
-        result = ""
-        i=0
-        while (result == "") and i < len(klist):
-            vset = klist[i]
-            i += 1
-            if c[i] in vset:
-                result = outvars[i]
-        if result == "":
-            result = outvars[-1]
-        
-        for i,o in enumerate(outvars):
-            cpt_row = []
-            cpt_row.extend(c)
-            cpt_row.append(o)
-            val = 1.0 if (o == result) else 0.0
-            cpt_row.append(val)
-            cpt_rows.append(cpt_row)
-    return (cpt_rows,keylist,outvars)
+        vdict = dictVarsAndValues(bayesianNetwork, cpt)
+        vlist = [vdict[v] for v in invars.keys()]
+        cartesian = list(itertools.product(*vlist))
+        klist = [a for a in invars.values()]
+        print('cartesian')
+        print(cartesian)
+        print('klist')
+        print(klist)
+        keylist = invars.keys()
+        cpt_rows = [] 
+        for c in cartesian:
+            result = ""
+            i=0
+            while (result == "") and i < len(klist):
+                vset = klist[i]
+                i += 1
+                if c[i] in vset:
+                    result = outvars[i]
+            if result == "":
+                result = outvars[-1]
 
-
+            for i,o in enumerate(outvars):
+                cpt_row = []
+                cpt_row.extend(c)
+                cpt_row.append(o)
+                val = 1.0 if (o == result) else 0.0
+                cpt_row.append(val)
+                cpt_rows.append(cpt_row)
+        return (cpt_rows,keylist,outvars)
 
 
 
-def addCpt(bayesianNetwork, cpt):
 
-    for name, cpt_tuple in cpt.items():
-        conditionalProbabilityTable = bayesianNetwork.conditionalProbabilityTables.add()
-        conditionalProbabilityTable.name = name
-        for rv in cpt_tuple[1]:
-            randomVariable = conditionalProbabilityTable.randomVariables.add()
-            randomVariable.name = rv
-        for row in cpt_tuple[0]:  
-            conditionalProbabilityRow = conditionalProbabilityTable.conditionalProbabilityRows.add()
-            for i,var in enumerate(row):
-              nvars = len(row)-1
-              if i < nvars:
-                randomVariableValue = conditionalProbabilityRow.randomVariableValues.add()
-                randomVariableValue.name = var
-              else:
-                conditionalProbabilityRow.probability = var
+
+    def addCpt(bayesianNetwork, cpt):
+
+        for name, cpt_tuple in cpt.items():
+            conditionalProbabilityTable = bayesianNetwork.conditionalProbabilityTables.add()
+            conditionalProbabilityTable.name = name
+            for rv in cpt_tuple[1]:
+                randomVariable = conditionalProbabilityTable.randomVariables.add()
+                randomVariable.name = rv
+            for row in cpt_tuple[0]:  
+                conditionalProbabilityRow = conditionalProbabilityTable.conditionalProbabilityRows.add()
+                for i,var in enumerate(row):
+                  nvars = len(row)-1
+                  if i < nvars:
+                    randomVariableValue = conditionalProbabilityRow.randomVariableValues.add()
+                    randomVariableValue.name = var
+                  else:
+                    conditionalProbabilityRow.probability = var
                     
         
 
 
-def bayesInitialize(bayesianNetwork,name):
-    model = BayesianNetwork(name)
-    state = {}
-    general_distribution = {}
-    for dist in bayesianNetwork.discreteDistributions:
-        distribution ={}
-        for var in dist.variables:
-            distribution[var.name]= var.probability
-        discreteDistribution = DiscreteDistribution(distribution)
-        general_distribution[dist.name] = discreteDistribution
-        state[dist.name] = Node(discreteDistribution, dist.name)
-        model.add_state(state[dist.name])
-    for table in bayesianNetwork.conditionalProbabilityTables:
-        tablelist = []
-        for row in table.conditionalProbabilityRows:
-            rowlist = []
-            for var in row.randomVariableValues:
-                rowlist.append (var.name)
-            rowlist.append(row.probability)
-            tablelist.append(rowlist)
-        varlist = []
-        for var in table.randomVariables:
-            varlist.append(general_distribution[var.name])
-        print("table.name")
-        print(table.name)
-        print("tablelist")
-        print(tablelist)
-        print("varlist")
-        print(varlist)
-        conditionalProbabilityTable = ConditionalProbabilityTable(tablelist,varlist)
-        general_distribution[table.name] = conditionalProbabilityTable
-        state[table.name] = Node(conditionalProbabilityTable, table.name)
-        model.add_state(state[table.name])
-        print('state')
-        print(state)
-        for var in table.randomVariables:
-            print("var.name")
-            print(var.name)
-            print ("table.name")
-            print (table.name)
-            model.add_edge(state[var.name],state[table.name])
-                
-    return model
+    def bayesInitialize(bayesianNetwork,name):
+        model = BayesianNetwork(name)
+        state = {}
+        general_distribution = {}
+        for dist in bayesianNetwork.discreteDistributions:
+            distribution ={}
+            for var in dist.variables:
+                distribution[var.name]= var.probability
+            discreteDistribution = DiscreteDistribution(distribution)
+            general_distribution[dist.name] = discreteDistribution
+            state[dist.name] = Node(discreteDistribution, dist.name)
+            model.add_state(state[dist.name])
+        for table in bayesianNetwork.conditionalProbabilityTables:
+            tablelist = []
+            for row in table.conditionalProbabilityRows:
+                rowlist = []
+                for var in row.randomVariableValues:
+                    rowlist.append (var.name)
+                rowlist.append(row.probability)
+                tablelist.append(rowlist)
+            varlist = []
+            for var in table.randomVariables:
+                varlist.append(general_distribution[var.name])
+            print("table.name")
+            print(table.name)
+            print("tablelist")
+            print(tablelist)
+            print("varlist")
+            print(varlist)
+            conditionalProbabilityTable = ConditionalProbabilityTable(tablelist,varlist)
+            general_distribution[table.name] = conditionalProbabilityTable
+            state[table.name] = Node(conditionalProbabilityTable, table.name)
+            model.add_state(state[table.name])
+            print('state')
+            print(state)
+            for var in table.randomVariables:
+                print("var.name")
+                print(var.name)
+                print ("table.name")
+                print (table.name)
+                model.add_edge(state[var.name],state[table.name])
+
+        return model
