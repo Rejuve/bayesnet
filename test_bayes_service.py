74c51fbb214583c3bf3faf606f8b2f38bc72b243
Deborah Duong <deborahvakasduong@gmail.com>
2020-07-22 18:42:05 -0700

    Update test_bayes_service.py

--- test_bayes_service.py
+++ test_bayes_service.py
@@ -10,6 +10,7 @@ from bayes.utils import create_query
 import service.service_spec.bayesian_pb2_grpc as grpc_bayes_grpc
 import service.service_spec.bayesian_pb2
 from service.service_spec.bayesian_pb2 import BayesianNetworkQuery
+from service.service_spec.bayesian_pb2 import QueryId
 
 from service import registry
 
@@ -27,45 +28,34 @@ if __name__ == "__main__":
         endpoint = input("Endpoint (localhost:{}): ".format(registry["bayes_service"]["grpc"])) if not test_flag else ""
         if endpoint == "":
             endpoint = "localhost:{}".format(registry["bayes_service"]["grpc"])
-        #print ("endpoint")
-        #print (endpoint)
         bayesianNetwork = covid_bayes.covid_bayes()
-        #print("bayesianNetwork")
-        #print(bayesianNetwork)
         evidence = {}
         outvars= ["emergency_treatment","covid_risk","covid_severity"]
         query = create_query(evidence,outvars,bayesianNetwork)
-        #print ("query")
-        #print (query)
-        bayesianNetworkQuery = BayesianNetworkQuery()
-        bayesianNetworkQuery.bayesianNetwork.CopyFrom( bayesianNetwork)
-        bayesianNetworkQuery.query.CopyFrom(query)
-        #print("bayesianNetworkQuery")
-        #print(bayesianNetworkQuery)
         
-        grpc_method = input("Method (net|query|both): ") if not test_flag else "both"
-        #print("grpc_method")
-        #print(grpc_method)
+        grpc_method = input("Method (stateless|statefull): ") if not test_flag else "statefull"
 
         # Open a gRPC channel
         channel = grpc.insecure_channel("{}".format(endpoint))
-        #print("channel")
-        #print(channel)
         stub = grpc_bayes_grpc.BayesNetStub(channel)
-        #print("stub")
-        #print(stub)
 
-        if grpc_method == "both":
+        if grpc_method == "stateless":
+            bayesianNetworkQuery = BayesianNetworkQuery()
+            bayesianNetworkQuery.bayesianNetwork.CopyFrom( bayesianNetwork)
+            bayesianNetworkQuery.query.CopyFrom(query)
             response = stub.StatelessNet(bayesianNetworkQuery)
-            #print("response")
-            #print(response)
-	
-            #print("response.varAnswers")
-            print(response.varAnswers)
-        elif grpc_method == "net":
-            pass
-        elif grpc_method == "query":
-           pass
+            print("response.varAnswers")
+	    print(response.varAnswers)
+        elif grpc_method == "statefull":
+            response = stub.StartNet(bayesianNetwork)
+	    print("response.id")
+	    print(response.id)
+	    queryId = QueryId()
+	    queryId.id = response.id
+	    queryId.query.CopyFrom(query)
+            response = stub.AskNet(queryId)
+	    print("response.varAnswers")
+	    print(response.varAnswers)
         else:
             print("Invalid method!")
             exit(1)
