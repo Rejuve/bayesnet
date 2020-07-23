b72d414e552ac6874656294502cd328e7ac122a1
Deborah Duong <deborahvakasduong@gmail.com>
2020-07-22 23:28:58 -0700

    Update bayes_service.py

--- service/bayes_service.py
+++ service/bayes_service.py
@@ -13,6 +13,7 @@ from bayes.utils import query
 from bayes.utils import get_evidence_and_outvars
 from bayes.utils import get_var_positions
 from bayes.utils import get_var_val_positions
+from bayes.utils import complexity_check
 import os
 import pickle
 import pomegranate
@@ -66,26 +67,30 @@ class BayesNetServicer(grpc_bt_grpc.BayesNetServicer):
     log.debug("BayesServicer created")
     
   def getUniqueID(self):
-    i=0
+    i=1
     while i in self.baked or i in self.spec:
       i += 1
     return i
     
 
   def StartNet(self, request, context):
-    uniqueID = self.getUniqueID()
-    self.spec[uniqueID]= request
-    self.baked[uniqueID] = bayesInitialize(request)
-    self.baked[uniqueID].bake()
-    self.spec_json = {}
-    #todo: return id asynchronously without waiting to save, or save if guaranteed upon error
-    for i, message in self.spec.items():
-      self.spec_json[i] = json_format.MessageToJson(message)
-    with open(self.spec_path, 'wb') as f:
-      pickle.dump(self.spec_json, f)
-      f.close()
     id = Id()
-    id.id = uniqueID
+    not_too_complex,error_msg = complexity_check(request)
+    if not_too_complex:
+      uniqueID = self.getUniqueID()
+      self.spec[uniqueID]= request
+      self.baked[uniqueID] = bayesInitialize(request)
+      self.baked[uniqueID].bake()
+      self.spec_json = {}
+      #todo: return id asynchronously without waiting to save, or save if guaranteed upon error
+      for i, message in self.spec.items():
+        self.spec_json[i] = json_format.MessageToJson(message)
+      with open(self.spec_path, 'wb') as f:
+        pickle.dump(self.spec_json, f)
+        f.close()
+      id.id = uniqueID
+    else:
+      id.error_msg = error_msg
     return id
   
 
@@ -109,25 +114,30 @@ class BayesNetServicer(grpc_bt_grpc.BayesNetServicer):
     return(answer)
 
   def StatelessNet(self, request, context):
-  
-    net= bayesInitialize(request.bayesianNetwork)
-    net.bake()
-    evidence,outvars = get_evidence_and_outvars(request.query, request.bayesianNetwork)
-    answer_dict = query(net, request.bayesianNetwork, evidence,outvars)
     answer = Answer()
-    var_positions = get_var_positions(request.bayesianNetwork)
-    var_val_positions = get_var_val_positions(request.bayesianNetwork)
-    
-    for var, val_dict in answer_dict.items():
-      var_answer = answer.varAnswers.add()
-      if var in var_positions:
-        var_num = var_positions[var]
-        var_answer.var_num = var_num
-        for val, prob in val_dict.items():
-          val_num = var_val_positions[var][val]
-          var_state = var_answer.varStates.add()
-          var_state.state_num = val_num
-          var_state.probability =prob
+    not_too_complex,error_msg = complexity_check(request.bayesianNetwork)
+    if not_too_complex:
+      net= bayesInitialize(request.bayesianNetwork)
+      net.bake()
+      evidence,outvars = get_evidence_and_outvars(request.query, request.bayesianNetwork)
+      answer_dict = query(net, request.bayesianNetwork, evidence,outvars)
+
+      var_positions = get_var_positions(request.bayesianNetwork)
+      var_val_positions = get_var_val_positions(request.bayesianNetwork)
+
+      for var, val_dict in answer_dict.items():
+        var_answer = answer.varAnswers.add()
+        if var in var_positions:
+          var_num = var_positions[var]
+          var_answer.var_num = var_num
+          for val, prob in val_dict.items():
+            val_num = var_val_positions[var][val]
+            var_state = var_answer.varStates.add()
+            var_state.state_num = val_num
+            var_state.probability =prob
+    else:
+      answer.error_msg = error_msg
+        
     return(answer)
         
 
