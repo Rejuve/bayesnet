67d7e9f503c532db6e7cb4edb9b37949641f2c8c
Deborah Duong <deborahvakasduong@gmail.com>
2020-07-27 04:41:12 -0700

    Update buildproto.sh

--- buildproto.sh
+++ buildproto.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. service/service_spec/bayesian.proto
+python3 -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. sn_service/service_spec/bayesian.proto
