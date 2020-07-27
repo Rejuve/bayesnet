0e3fd56dd2ccbedc62ad5191644ac28ead33b775
Deborah Duong <deborahvakasduong@gmail.com>
2020-07-27 03:43:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 
 setup(name='covid-bayes',
       version='1.0',
-      packages=['covid-bayesnet','covid-bayesnet.bayes','covid-bayesnet.service']
+      packages=['bayes','service']
       )
