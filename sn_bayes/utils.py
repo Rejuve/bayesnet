bdd895eff3a9f09201237ade43e52e2024ccc351
Deborah Duong <deborahvakasduong@gmail.com>
2020-08-29 21:53:15 -0700

    Update utils.py

--- sn_bayes/utils.py
+++ sn_bayes/utils.py
@@ -219,7 +219,7 @@ def explain(baked_net, netspec, evidence,explain_list, reverse_explain_list = []
 	#reverse_evidence_list tells which of the evidence to explain should perturb one val to the left rather than the right (the default)
 	
 	#first make a list of all the pertubations to make
-	evidence_pertubations = []
+	evidence_perturbations = []
 	var_val_positions = get_var_val_positions(netspec)
 	var_val_names = get_var_val_names(netspec)
 	for var,val in evidence.items():
@@ -233,7 +233,7 @@ def explain(baked_net, netspec, evidence,explain_list, reverse_explain_list = []
 			new_evidence = copy.deepcopy(evidence)
 			new_val = var_val_names[var][new_pos]
 			new_evidence[var]=new_val
-			evidence_pertubations.append(new_evidence)
+			evidence_perturbations.append(new_evidence)
 	
 	#next run each, obtaining the values of vars to be explained.  
 	#find the difference between these outputvalues and the output values from the original evidence input
@@ -246,7 +246,7 @@ def explain(baked_net, netspec, evidence,explain_list, reverse_explain_list = []
 		winners[key] = (winner,winner_val)
 		max_diff[key] = (winner,0)
 	
-	for evidence in evidence_perterbations:
+	for evidence in evidence_perturbations:
 		result = query(baked_net,netspec,evidence,explain_list)
 		for key,val_tuple in explain_list:
 			diff = result[key][winners[key][0]]-winners[key][1] if key in reverse_explain_list else winners[
